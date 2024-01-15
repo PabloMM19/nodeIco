fbd5d230383a2c39649dc564b2b9996c4d489b37
PabloMolero <pmoleromarin@gmail.com>
2024-01-15 11:49:07 +0100

    nuevo

--- dist/app.js
+++ dist/app.js
@@ -1,43 +1,31 @@
-import express, {urlencoded} from "express";
-
+import express, { urlencoded } from "express";
 import { rutas } from "./utils/rutas.js";
 import { adminRouter } from "./routes/adminRoutes.js";
 import { shopRouter } from "./routes/shopRoutes.js";
-
 console.log('------------------------------------------------------------_---');
 console.log("Bienvenido a mi app");
-
-const port =  3000;
-
+const port = 3000;
 const app = express();
-
-app.use(urlencoded({extended: false})); //Middleware para procesar los campos que me envíen por HTTP body-parser
+app.use(urlencoded({ extended: false })); //Middleware para procesar los campos que me envíen por HTTP body-parser
 app.use(express.static(rutas.public)); //Mia rutas contenido estáticos .css .js
 app.disable('x-powered-by');
 app.set('view engine', 'ejs');
-app.set('views',rutas.views); //CAMBIAR
-
+app.set('views', rutas.views); //CAMBIAR
 app.use('/admin', adminRouter); //Las rutas empiezan por /admin
 app.use('/', shopRouter);
 //Controladores para responder a las peticiones por HTTP
-
-
-
-
-app.use('/coche',(req, res, next) => {
+app.use('/coche', (req, res, next) => {
     console.log("Ha llegado una petición");
     next();
 });
-app.use('/coche', (req,res,next) => {
+app.use('/coche', (req, res, next) => {
     console.log("Estamos en el segundo middleware");
-    res.send({"message":"ok"});
+    res.send({ "message": "ok" });
 });
-
-app.use('/', (req,res,next)=> {
+app.use('/', (req, res, next) => {
     console.log("Middleware del final");
-    res.status(404).send({'error':'Ruta no encontrada'});
-})
-
+    res.status(404).send({ 'error': 'Ruta no encontrada' });
+});
 // FIN 
 app.listen(port);
 console.log("Servidor de la app en marcha");
---
+++ src/routes/adminRoutes.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+
+export const adminRouter = Router();
+
+export const productos: {producto: String}[] = [];
+
+//todas las rutas que llegan aquí empiezan por /admin
+
+adminRouter.get('/add-product',(req,res,next)=>{
+    console.log("Devolvemos el formulario para meter productos");
+    //res.send('<FORM action="/admin/product" method="POST"> <input type="text" name="producto"> <button type="submit"> Enviar </button> </FORM> ');
+    res.render('add-product', {pageTitle:'Añadir producto', path:'/admin/add-product'});
+});
+
+adminRouter.post('/add-product', (req, res, next) => {
+    if (req.body.producto) {
+        console.log('Ha llegado el siguiente producto: ', req.body.producto);
+        productos.push({producto: req.body.producto});
+    }
+    res.redirect('/');
+});
+
